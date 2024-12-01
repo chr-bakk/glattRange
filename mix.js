464680f080905dd717d2c598d45f0f45a5b24c1f
Christian Stene Bakkhaug <bakkiz@gmail.com>
2024-12-01 21:53:04 +0100

    Create mix.js

---
+++ mix.js
@@ -0,0 +1,55 @@
+// version dunno
+const NULL = ['nulk','minMaxNah']
+const touchSliders = NULL
+class MouseInputs {
+	constructor(min,max,call) {
+	}
+	static ctrl = false
+	static numberInputs = {
+		// number: {
+		// 	min: -100,
+		// 	max: 1000,
+		// 	call: MouseInputs.update,
+		// },
+	}
+	static ranges = {
+	}
+	static addEvents() {
+		Object.keys(this.numberInputs).forEach((input) => {
+			document.getElementById(input).addEventListener('wheel', e => {MouseInputs.numberInput(e, input)})
+		})
+		Object.keys(this.ranges).forEach((input) => {
+			document.getElementById(input).addEventListener('wheel', e => {MouseInputs.rangeInput(e, input)})
+		})
+		document.addEventListener('keydown', function(event) {if (event.ctrlKey) {MouseInputs.ctrl = true}})
+		document.addEventListener('keyup', function(event) {if (!event.ctrlKey) {MouseInputs.ctrl = false}})
+	}
+	static setLimits() {
+	}
+	static rangeInput(e, input) {
+		e.preventDefault()
+		const wheel = e.deltaY>0?this.ctrl?-10:-1:this.ctrl?10:1
+		const val = Number(e.target.value)+wheel
+		const {min, max, call} = MouseInputs.ranges[input]
+		if (val < min || val == max+1) {return}
+		call(val,true)
+	}
+	static numberInput(e, input) {
+		e.preventDefault()
+		const wheel = e.deltaY>0?this.ctrl?-10:-1:this.ctrl?10:1
+		const val = Number(e.target.value)+wheel
+		e.target.value = val
+		const {min, max, call} = MouseInputs.numberInputs[input]
+		if (val < min || val == max+1) {return}
+		call()
+	}
+	static update() {
+		Main.clear()
+		const n = Number(window.phi.value)
+		const m = Number(window.rho.value)
+		const layers = Number(window.layers.value)
+		const number = Number(window.number.value)
+		Main.swirls(n,m,layers,number)
+	}
+}
+MouseInputs.addEvents()
